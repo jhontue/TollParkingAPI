029a8b567499a768bc7ca57505b771bce81e5efd
Eduardo ORE HERRERA <jhontue@gmail.com>
2019-05-28 20:38:47 +0200

    add testing of PerHourAndFixedPricingPolicy

--- src/test/java/com/jhontue/parking/api/internal/PerHourAndFixedAmountPricingPolicyTest.java
+++ src/test/java/com/jhontue/parking/api/internal/PerHourAndFixedAmountPricingPolicyTest.java
@@ -6,12 +6,12 @@ import java.math.BigDecimal;
 import java.time.format.DateTimeFormatter;
 
 import static java.time.LocalDateTime.parse;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 /**
- * Test of {@link PerHourPricingPolicy}
+ * Test of {@link PerHourAndFixedAmountPricingPolicy}
  */
-public class PerHourPricingPolicyTest {
+public class PerHourAndFixedAmountPricingPolicyTest {
 
     /**
      * Date formatter
@@ -20,14 +20,14 @@ public class PerHourPricingPolicyTest {
 
     @Test
     public void computePrice() {
-        PerHourPricingPolicy pricingPolicy = new PerHourPricingPolicy(new BigDecimal("2.00"));
+        PerHourAndFixedAmountPricingPolicy pricingPolicy = new PerHourAndFixedAmountPricingPolicy(new BigDecimal(5), new BigDecimal(10));
 
         BigDecimal twoHoursPrice = pricingPolicy.computePrice(parse("2016-11-09 10:00", FORMATTER), parse("2016-11-09 12:00", FORMATTER));
         BigDecimal halfHourPrice = pricingPolicy.computePrice(parse("2016-11-09 10:30", FORMATTER), parse("2016-11-09 11:00", FORMATTER));
         BigDecimal tenMinPrice = pricingPolicy.computePrice(parse("2016-11-09 10:50", FORMATTER), parse("2016-11-09 11:00", FORMATTER));
 
-        assertEquals(new BigDecimal("4.00"), twoHoursPrice);
-        assertEquals(new BigDecimal("1.00"), halfHourPrice);
-        assertEquals(new BigDecimal("0.34"), tenMinPrice);
+        assertEquals(new BigDecimal("20.00"), twoHoursPrice);
+        assertEquals(new BigDecimal("12.50"), halfHourPrice);
+        assertEquals(new BigDecimal("10.85"), tenMinPrice);
     }
 }
